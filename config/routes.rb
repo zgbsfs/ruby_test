df746fd970a1191677c3204020b823361ca5eba6
zgbsfs <zgbsfs@hotmail.com>
2016-03-07 13:46:24 +0800

    final

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
-Rails.application.routes.draw do
-  resources :books
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,4 +52,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+Rails.application.routes.draw do
+  devise_for :users
+	  get '/about', to: 'static_pages#about'
+	  resources :books
 end
